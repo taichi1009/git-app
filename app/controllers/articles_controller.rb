59bde352a6fe690dd9c975a5ee6cdad8c377859a
Taichi Koido <taichi1009@gmail.com>
2016-07-21 18:35:51 +0900

    deleted unnecessary comments

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -67,7 +67,6 @@ class ArticlesController < ApplicationController
       @article = Article.find(params[:id])
     end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
     def article_params
       params.require(:article).permit(:title, :author, :body)
     end
